d5e64a3073adff1df8d97f85f45df8c5d6b4096e
jonathan-farah <105609646+jonathan-farah@users.noreply.github.com>
2024-11-10 11:13:11 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt09-C6-array-multiplier(
+  tt_um_jonathan-farah(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
