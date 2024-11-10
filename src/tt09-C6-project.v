068fdc462b74a45b35149533ad56f779fa00a697
jonathan-farah <105609646+jonathan-farah@users.noreply.github.com>
2024-11-10 10:57:37 -0500

    Update tt09-C6-project.v

--- src/tt09-C6-project.v
+++ src/tt09-C6-project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt09-C6-project. (
+module tt09-C6-project(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
